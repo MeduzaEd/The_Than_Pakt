66a515bc5d9be4e403100aaf5f1c8ceb8b62e5cb
DESKTOP-C2VGQU4\BeatrixDesktop <onlyadkortank@gmail.com>
2023-11-06 22:59:53 +0300

    Big Commint 0.1f

--- Assets/_Main_Scripts_/_Character/_Character_Controller_.cs
+++ Assets/_Main_Scripts_/_Character/_Character_Controller_.cs
@@ -13,18 +13,21 @@ public class _Character_Controller_ : NetworkBehaviour
     [SerializeField]
     Animator _Animator;
     [SerializeField]
-    private Vector3 Offset = new Vector3(-0.1f, 2f, -2f);
+    private Vector3 Offset = new Vector3(-0.1f, 1f, -2f);
     public float MaxVerticalAngle = 80.0f; // Максимальный угол наклона вверх
     public float MinVerticalAngle = -80.0f; // Максимальный угол наклона вниз
 
-    public float CameraZoom = 1f;
+    public float CameraZoomSpeed = 0.25f;
+    public float CameraZoomMin = 1f;
     public float CameraZoomMax = 2f;
     public float CameraSpeed = 5f;
     public float cameraspeed = 5f;
     [SerializeField]
     private float verticalRotation = 0.0f;
     private Humanoid hum;
-
+    private Vector2 touchStart;
+    private FixedJoystick FJ;
+    float ZoomInput = 0;
     [SyncVar]
     public bool InputLag = false;
     private void Start()
@@ -32,44 +35,83 @@ public class _Character_Controller_ : NetworkBehaviour
         hum = this.GetComponentInChildren<Humanoid>();
         RB = this.GetComponentInChildren<Rigidbody>();
         C = GameObject.FindObjectOfType<Camera>();
+        FJ = GameObject.FindObjectOfType<FixedJoystick>();
+        ZoomInput = -10f;
     }
     void Update()
     {
         if (!isLocalPlayer || !isClient) { return; }
-        float ZoomInput = Input.GetAxis("Mouse ScrollWheel");
-        Debug.Log(ZoomInput);
+        
+        if (SystemInfo.deviceType != DeviceType.Handheld)
+        {
+             ZoomInput = Input.GetAxis("Mouse ScrollWheel")* ((CameraZoomSpeed)*25f);
+        }
+        else
+        {
+
+            if (Input.touchCount == 2)
+            {
+                // MOBILE 
+                Touch touch1 = Input.GetTouch(0);
+                Touch touch2 = Input.GetTouch(1);
+
+                // Рассчитайте текущее расстояние между пальцами
+                float currentDistance = Vector2.Distance(touch1.position, touch2.position);
+
+                // Рассчитайте начальное расстояние между пальцами
+                float startDistance = Vector2.Distance(touch1.position - touch1.deltaPosition, touch2.position - touch2.deltaPosition);
+
+                // Рассчитайте изменение масштаба
+                float zoomAmount = (startDistance - currentDistance) * CameraZoomSpeed;
+
+                // Примените изменение масштаба с ограничениями
+                ZoomInput = zoomAmount;
+                Debug.Log(zoomAmount);
+            }
+        }
+        if (ZoomInput != 0)
+        {
+            Offset=Vector3.Lerp(Offset, new Vector3(-0.1f, Mathf.Clamp(Offset.y + ZoomInput, 0.1f, CameraZoomMax * 0.25f), Mathf.Clamp((Offset.z) + ZoomInput, CameraZoomMin, CameraZoomMax)), Time.deltaTime*125f);
+        }
+        ZoomInput = 0;
+
     }
     public void FixedUpdate()
     {
       
         if (!isLocalPlayer || !isClient) { return; }//is Not Server Check
         _Animator.SetFloat("Speed", RB.velocity.magnitude);
-        if (Input.GetAxis("Horizontal") != 0 || 0 != Input.GetAxis("Vertical"))
+        if (Input.GetAxis("Horizontal") != 0 || 0 != Input.GetAxis("Vertical") || FJ.Horizontal != 0 || FJ.Vertical != 0)
         {
             if (!RB) { return; }
 
                 float horizontalInput = Input.GetAxis("Horizontal");// X
                 float verticalInput = Input.GetAxis("Vertical");// Z
-                
+                if(FJ.Horizontal!=0|| FJ.Vertical != 0) { horizontalInput = FJ.Horizontal; verticalInput = FJ.Vertical; }
                 MoveOn(horizontalInput, verticalInput);
                // Debug.Log("isLocalMove: OnStart");
         }
-
     }
     public void LateUpdate()
     {
         if (!isLocalPlayer || !isClient) { return; }//is Not Server Check
+        float horizontalInput = Input.GetAxis("HorizontalRotation"); // Получаем ввод для вращения
+        float verticalInput = Input.GetAxis("VerticalRotation");
+
         if (SystemInfo.deviceType == DeviceType.Handheld)
         {
+            if (Input.touchCount == 2 && Input.GetAxis("Horizontal")==0 && Input.GetAxis("Vertical")==0)
+            {
+                horizontalInput = 0f;
+                verticalInput = 0f;
+            }
             if (Input.GetMouseButton(0)) { cameraspeed = CameraSpeed; } else { cameraspeed = 0; }
         }
         else
         {
             if (Input.GetMouseButton(1)) { cameraspeed = CameraSpeed; } else { cameraspeed = 0; }
         }
-        float horizontalInput = Input.GetAxis("HorizontalRotation"); // Получаем ввод для вращения
-        float verticalInput = Input.GetAxis("VerticalRotation");
-       
+
         Vector3 Rbp = RB.transform.position;
        
 
